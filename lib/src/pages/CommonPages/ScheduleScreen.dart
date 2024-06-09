c9ca099b6b226a58a355bc1cabd447eb1747ffd2
Vinicius Togni <tognivini@hotmail.com>
2024-06-09 20:55:24 -0300

    update files

--- lib/src/pages/CommonPages/ScheduleScreen.dart
+++ lib/src/pages/CommonPages/ScheduleScreen.dart
@@ -1,12 +1,18 @@
 // ignore_for_file: file_names, avoid_print, override_on_non_overriding_member
 
 import 'package:flutter/material.dart';
-import 'package:lc_mobile_flutter/src/components/SelectInput/SelectInput';
+
+const List<String> list = <String>[
+  'Lavanderia 1 - CEU 1',
+  'Lavanderia 2 - CEU 1',
+  'Lavanderia 1 - CEU 2'
+];
+// import 'package:lc_mobile_flutter/src/components/SelectInput/SelectInput';
 
 // ignore: must_be_immutable
 class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key, String? name});
-  static const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
   // String id;
 
   @override
@@ -24,6 +30,28 @@ class _ScheduleState extends State<ScheduleScreen> {
   TextEditingController phoneController = TextEditingController();
   TextEditingController passController = TextEditingController();
   String dropdownValue = list.first;
+
+  //Method for showing the date picker
+  void _pickDateDialog() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            //which date will display when user open the picker
+            firstDate: DateTime(1950),
+            //what will be the previous supported year in picker
+            lastDate: DateTime
+                .now()) //what will be the up to supported date in picker
+        .then((pickedDate) {
+      //then usually do the future job
+      if (pickedDate == null) {
+        //if user tap cancel then this function will stop
+        return;
+      }
+      setState(() {
+        //for rebuilding the ui
+      });
+    });
+  }
 
   // setState(
   // nomeController.text
@@ -46,25 +74,37 @@ class _ScheduleState extends State<ScheduleScreen> {
               ),
               Padding(
                   padding: const EdgeInsets.only(
-                      top: 10, right: 20, bottom: 10, left: 20),
-                  child: TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                        hintText: 'Digite um e-mail válido por favor'),
-                  )),
-              Padding(
-                  padding: const EdgeInsets.only(
-                      top: 10, right: 20, bottom: 10, left: 20),
-                  child: TextFormField(
-                    keyboardType: TextInputType.phone,
-                    controller: phoneController,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Telefone',
-                        hintText: '(88) 8 xxxx-xxxx'),
+                      top: 10, left: 20, right: 20, bottom: 0),
+                  child: Container(
+                    height: 80,
+                    width: 280,
+                    decoration: BoxDecoration(
+                        // color: Colors.blue,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(
+                          color: Color.fromARGB(255, 29, 28, 31)),
+                      padding: const EdgeInsets.only(top: 0, bottom: 20),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? value) {
+                        // This is called when the user selects an item.
+                        setState(() {
+                          dropdownValue = value!;
+                        });
+                      },
+                      items: list.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
                   )),
               Padding(
                   padding: const EdgeInsets.only(
