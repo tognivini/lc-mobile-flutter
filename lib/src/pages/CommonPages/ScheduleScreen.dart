8cfec10026dd8cf0b7b267aec7fbc798d92907a2
Vinicius Togni <tognivini@hotmail.com>
2024-06-13 18:55:03 -0300

    update files, add calendar button

--- lib/src/pages/CommonPages/ScheduleScreen.dart
+++ lib/src/pages/CommonPages/ScheduleScreen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: file_names, avoid_print, override_on_non_overriding_member
+// ignore_for_file: file_names, avoid_print, override_on_non_overriding_member, deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 const List<String> list = <String>[
   'Lavanderia 1 - CEU 1',
@@ -20,6 +21,18 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleState extends State<ScheduleScreen> {
+  var finaldate;
+
+  final ButtonStyle style =
+      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 10));
+
+  void callDatePicker() async {
+    var order = await getDate();
+    setState(() {
+      finaldate = order;
+    });
+  }
+
   // ScheduleScreen({
   //   super.key,
   //   required this.id,
@@ -31,31 +44,26 @@ class _ScheduleState extends State<ScheduleScreen> {
   TextEditingController passController = TextEditingController();
   String dropdownValue = list.first;
 
-  //Method for showing the date picker
-  void _pickDateDialog() {
-    showDatePicker(
-            context: context,
-            initialDate: DateTime.now(),
-            //which date will display when user open the picker
-            firstDate: DateTime(1950),
-            //what will be the previous supported year in picker
-            lastDate: DateTime
-                .now()) //what will be the up to supported date in picker
-        .then((pickedDate) {
-      //then usually do the future job
-      if (pickedDate == null) {
-        //if user tap cancel then this function will stop
-        return;
-      }
-      setState(() {
-        //for rebuilding the ui
-      });
-    });
-  }
-
   // setState(
   // nomeController.text
   // )
+
+  Future<DateTime?> getDate() {
+    // Imagine that this function is
+    // more complex and slow.
+    return showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2018),
+      lastDate: DateTime(2030),
+      // builder: (BuildContext context, Widget child) {
+      //   return Theme(
+      //     data: ThemeData.light(),
+      //     child: child,
+      //   );
+      // },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -118,27 +126,83 @@ class _ScheduleState extends State<ScheduleScreen> {
                         hintText: 'Digite uma senha válida por favor'),
                   )),
               Padding(
-                  padding: const EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                child: Container(
+                    height: 50,
+                    width: 100,
+                    // decoration: BoxDecoration(
+                    //     color: Colors.blue,
+                    //     borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        finaldate == null
+                            ? const Text(
+                                "Use",
+                                textScaleFactor: 1.0,
+                              )
+                            : Text(
+                                "$finaldate",
+                                textScaleFactor: 1.0,
+                              ),
+                        const SizedBox(height: 15),
+                        // Padding(
+                        //   padding: const EdgeInsets.only(top: 20, bottom: 20),
+                        //   child: ElevatedButton(
+                        //     child: Text('Button'),
+                        //     style: ElevatedButton.styleFrom(
+                        //         backgroundColor: Colors.blue),
+                        //     onPressed: () {
+                        //       print('____ping____');
+                        //       // callDatePicker();
+                        //     },
+                        //   ),
+                        // ),
+                      ],
+                    )),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 5),
                   child: Container(
                     height: 50,
                     width: 100,
                     decoration: BoxDecoration(
                         color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20)),
+                        borderRadius: BorderRadius.circular(5)),
                     child: TextButton(
                       style: ButtonStyle(
                         foregroundColor:
                             MaterialStateProperty.all<Color>(Colors.white),
                       ),
                       onPressed: () {
-                        print(nameController.text);
-                        print(matriculaController.text);
-                        print(emailController.text);
-                        print(passController.text);
+                        callDatePicker();
                       },
-                      child: const Text('Editar'),
+                      child: const Text('Agendar'),
                     ),
                   )),
+
+              // Padding(
+              //     padding: const EdgeInsets.only(top: 5),
+              //     child: Container(
+              //       height: 50,
+              //       width: 100,
+              //       decoration: BoxDecoration(
+              //           color: Colors.blue,
+              //           borderRadius: BorderRadius.circular(5)),
+              //       child: TextButton(
+              //         style: ButtonStyle(
+              //           foregroundColor:
+              //               MaterialStateProperty.all<Color>(Colors.white),
+              //         ),
+              //         onPressed: () {
+              //           print(nameController.text);
+              //           print(matriculaController.text);
+              //           print(emailController.text);
+              //           print(passController.text);
+              //         },
+              //         child: const Text('Agendar'),
+              //       ),
+              //     )),
             ],
           ),
         ));
